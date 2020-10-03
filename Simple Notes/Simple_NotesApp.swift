c76d17cf763e189720c14b9925838c68cf164a72
Bohdan Podvirnyi <bohdan.podvirnyi@gmail.com>
2020-10-03 15:25:36 +0300

    Initial Commit

---
+++ Simple Notes/Simple_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_NotesApp.swift
+//  Simple Notes
+//
+//  Created by Bohdan on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Simple_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
